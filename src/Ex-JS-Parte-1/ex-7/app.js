50f9712ff186e0c21b7ea5bbef2d7cb8379ac295
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-01-05 19:50:33 +0100

    ejercicio 7 terminado

--- src/Ex-JS-Parte-1/ex-7/app.js
+++ src/Ex-JS-Parte-1/ex-7/app.js
@@ -1,2 +1,11 @@
-var text = prompt("Escribe una frase");
-var nText = text.length;
+let text = prompt("Escribe una frase");
+let caracterA = "a"
+let contadorLetras = 0
+
+
+for (let i = 0; i < text.length; i++){
+    if(text.charAt(i) === caracterA) {
+        contadorLetras++;
+    }
+}
+console.log("Aparece" + contadorLetras + " veces la letra" + " a "     )
