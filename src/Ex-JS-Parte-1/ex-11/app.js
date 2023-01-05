54026408268ba0c89b94eb36dd24ca30c8775d6a
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-01-05 20:48:47 +0100

    ejercicio 11 terminado

---
+++ src/Ex-JS-Parte-1/ex-11/app.js
@@ -0,0 +1,9 @@
+const numero = prompt("Dime un número")
+const resultado = function saberSiEsDivisible() {
+    if (numero % 5 === 0  ) { alert("El número es divisible entre 5")
+    }
+    else { 
+        alert("El número no es divisible entre 5")
+    }
+}
+resultado()
