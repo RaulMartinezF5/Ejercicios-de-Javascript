52e60748a55cfa7b92fe87012f110686c0161bbe
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-01-06 11:32:32 +0100

    ejercicio 13 terminado

---
+++ src/Ex-JS-Parte-1/ex-13/app.js
@@ -0,0 +1,15 @@
+function obtenerDivisores(numero) {
+    
+    const divisores = [];
+
+    for (let i = 1; i <= numero; i++) {
+      if (numero % i == 0) {
+        divisores.push(i);
+      }
+    }
+    return divisores;
+  }
+  
+  const numero = prompt("Ingresa un número:");
+  
+  alert("Los divisores de " + numero + " son: " + obtenerDivisores(numero));
